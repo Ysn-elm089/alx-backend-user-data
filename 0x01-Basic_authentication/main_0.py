0c8c1c734d16007c4e541a9f8eca1d212fae60e4
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2024-11-24 16:23:06 +0000

    fix error

--- 0x01-Basic_authentication/main_0.py
+++ 0x01-Basic_authentication/main_0.py
@@ -1,32 +1,10 @@
-#!/usr/bin/python3
-""" Check response
+#!/usr/bin/env python3
+""" Main 0
 """
-import requests
+from api.v1.auth.auth import Auth
 
-if __name__ == "__main__":
-    r = requests.get('http://0.0.0.0:3456/api/v1/status')
-    if r.status_code != 200:
-        print("Wrong status code: {}".format(r.status_code))
-        exit(1)
-    if r.headers.get('content-type') != "application/json":
-        print("Wrong content type: {}".format(r.headers.get('content-type')))
-        exit(1)
-    
-    try:
-        r_json = r.json()
-        
-        if len(r_json.keys()) != 1:
-            print("Not the right number of element in the JSON: {}".format(r_json))
-            exit(1)
-        
-        status_value = r_json.get('status')
-        if status_value is None:
-            print("Missing 'status' key in the JSON: {}".format(r_json))
-            exit(1)
-        if status_value != "OK":
-            print("'status' doesn't have the right value: {}".format(status_value))
-            exit(1)
-            
-        print("OK", end="")
-    except:
-        print("Error, not a JSON")
+a = Auth()
+
+print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
+print(a.authorization_header())
+print(a.current_user())
